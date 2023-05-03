25ea34d1228d652e44ac0e3477610f26b5eb5a3c
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-05-03 20:34:48 +0300

    feat: added seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
+import image from "@astrojs/image";
 import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'httpls://astro-website.netllfy.app',
   integrations: [react(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
